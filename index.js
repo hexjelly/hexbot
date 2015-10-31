3d73b2c2e0c6acd0961546bf075e0f778dbae9f4
Roger Andersen <hexjelly@hexjelly.com>
2015-10-31 09:00:32 +0100

    Add error handler, update config file

--- index.js
+++ index.js
@@ -2,14 +2,24 @@
 'use strict';
 
 
-/* core requires, initialization, connect */
+// core requires, initialization, connect
 var irc = require('irc');
 var nconf = require('nconf');
 
 nconf.file({file: 'config/config.default.json'});
 nconf.defaults({});
 
-var bot = new irc.Client(nconf.get('server'), nconf.get('botName'), { channels: nconf.get('channels'), floodProtection: true });
+var bot = new irc.Client(
+  nconf.get('hexbot').server,
+  nconf.get('hexbot').botName,
+  nconf.get('irc')
+);
 
 
-/* load plugins */
+// error handler
+bot.addListener('error', function(message) {
+    console.log('error: ', message);
+});
+
+
+// load plugins
