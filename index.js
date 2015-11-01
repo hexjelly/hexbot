a4fc41ae8618f3223d5499f75ff33572b35a3bfd
Roger Andersen <hexjelly@hexjelly.com>
2015-11-01 03:18:36 +0100

    Create Elma WR plugin

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-/*jslint node: true */
 'use strict';
-
 
 // core requires, initialization, connect
 var irc = require('irc');
 var nconf = require('nconf');
-
 nconf.file({file: 'config/config.default.json'});
 nconf.defaults({});
 
@@ -15,10 +12,15 @@ var bot = new irc.Client(
   nconf.get('irc')
 );
 
+console.log('connecting to %s ...', nconf.get('hexbot').server);
 
-// error handler
+// core handlers
 bot.on('error', function (message) {
   console.log('error: ', message);
+});
+
+bot.on('registered', function (message) {
+  console.log('connected!');
 });
 
 
---
+++ plugins/elma.wrs.js
@@ -0,0 +1,31 @@
+/* Get current world record for Elasto Mania internal levels */
+
+'use strict';
+
+module.exports = (function () {
+  return function init(bot) {
+    var regex = /^!wr\s*0*(\d+)$/i; // matches lines starting with '!wr' followed by optional whitespace, optional 0s, then digit(s)
+
+    bot.on('message', function(from, to, text) {
+      if (to === bot.nick) { // pm instead of channel
+        to = from;
+      }
+      var result = re.exec(text);
+      if (result && result[1] <= 54) {
+        bot.say(to,getWR(result[1]));
+      }
+    });
+  };
+  function getWR(n) {
+    var request = require('request');
+    var cheerio = require('cheerio');
+    var url = 'http://www.moposite.com/records_elma_wrs.php';
+    // no point parsing names of levels from the page as they won't change and we already know them
+    var internals = ['Warm Up','Flat Track','Twin Peaks','Over and Under','Uphill Battle','Long Haul',
+                    'Hi Flyer','Tag','Tunnel Terror','The Steppes','Gravity Ride','Islands in the Sky',
+                    'Hill Legend','Loop-de-Loop','Serpents Tale','New Wave','Labyrinth','Spiral',
+                    'Turnaround','Upside Down','Hangman','Slalom','Quick Round','Ramp Frenzy','Precarious',
+                    'Circuitous','Shelf Life','Bounce Back','Headbanger'];
+    return n + '. ' + internals[n+1] + ': ';
+  };
+})();
