749e0eba5ea1d7e0982f467a725f54472b1bef47
Roger Andersen <hexjelly@hexjelly.com>
2015-11-06 03:45:47 +0100

    Update calc.js trigger

--- plugins/calc.js
+++ plugins/calc.js
@@ -4,15 +4,15 @@
 
 module.exports = (function () {
   return function init(bot) {
-    var regex = /^!calc\s+(.+)/i;
+    var regex = /^!(calc|c)\s+(.+)/i;
 
     bot.on('message', function(from, to, text) {
       if (to === bot.nick) { // pm instead of channel
         to = from;
       }
       var result = regex.exec(text);
-      if (result && result[1]) {
-        calculate(result[1], bot, to); // thanks to async hell, no idea how to do this better?
+      if (result && result[2]) {
+        calculate(result[2], bot, to); // thanks to async hell, no idea how to do this better?
       }
     });
   };
