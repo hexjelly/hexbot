aeff4af9f54044ff3220113cc0c0f3b33515001c
Roger Andersen <hexjelly@hexjelly.com>
2015-11-09 19:05:13 +0100

    Add wikipedia.js

---
+++ plugins/wikipedia.js
@@ -0,0 +1,51 @@
+/* Get Wikipedia information */
+
+'use strict';
+
+module.exports = (function () {
+  return function init(bot) {
+    var regex = /^!wiki?\s+(.+)/i;
+
+    bot.on('message', function(from, to, text) {
+      if (to === bot.nick) { // pm instead of channel
+        to = from;
+      }
+      var result = regex.exec(text);
+      if (result) {
+        wiki(result[1], bot, to); // thanks to async hell, no idea how to do this better?
+      }
+    });
+  };
+
+  function wiki(article, bot, to) {
+    var request = require('request');
+    var url = "https://en.wikipedia.org/w/api.php?format=json&action=opensearch&namespace=0&search=" + article + "&limit=1";
+
+    request(url, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        var wiki = JSON.parse(body);
+        if (wiki[1] != '') { // there's a match for our search term
+          var url = "https://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exchars=450&exintro=&explaintext=&exsectionformat=plain&titles=" + wiki[1] + "&redirects=";
+          request(url, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+              var wiki = JSON.parse(body).query.pages;
+              wiki = wiki[Object.keys(wiki)[0]];
+              if (!wiki.hasOwnProperty('missing')) {
+                var title = wiki.title;
+                var url = ' - ' + encodeURI("https://en.wikipedia.org/wiki/" + title);
+                var extract = wiki.extract.replace(/\n/g, ", "); // wiki results gives us newlines now and then
+                if (url.length + extract.length > 420) {
+                  // IRC message length limit is 512, take nick/host into account (arbitrarily atm) and shorten extract to fit together with URL
+                  extract = extract.substr(0,417-url.length) + '...';
+                }
+                bot.say(to, extract + url);
+              }
+            }
+          });
+        } else { // no search result
+          bot.say(to, 'No result for "' + article + '".');
+        }
+      }
+    });
+  };
+})();
