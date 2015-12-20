71a75108d67a31f4c009efda3126f0d76807c421
Roger Andersen <hexjelly@hexjelly.com>
2015-12-21 00:10:31 +0100

    Add !imdb search to imdb.js, use akas.imdb.com

--- plugins/imdb.js
+++ plugins/imdb.js
@@ -4,15 +4,17 @@
 
 module.exports = (function () {
   return function init(bot) {
-    var regex = /.*imdb\.com\/title\/tt(\d{7}).*/i;
+    var regex = /(!imdb)\s(.*)|.*imdb\.com\/title\/tt(\d{7}).*/i;
 
     bot.on('message', function(from, to, text) {
       if (to === bot.nick) { // pm instead of channel
         to = from;
       }
       var result = regex.exec(text);
-      if (result && result[1]) {
+      if (result && result[1] != "!imdb") {
         getIMDb(result[1], bot, to); // thanks to async hell, no idea how to do this better?
+      } else if (result && result[1] && result[2]) {
+        searchIMDb(result[2], bot, to);
       }
     });
   };
@@ -20,7 +22,7 @@ module.exports = (function () {
   function getIMDb(tt, bot, to) {
     var request = require('request');
     var cheerio = require('cheerio');
-    var url = 'http://www.imdb.com/title/tt' + tt;
+    var url = 'http://akas.imdb.com/title/tt' + tt;
     var titleregex = /^(.*)\s(\(.*\))\s-\sIMDb$/;
 
     request(url, function (error, response, body) {
@@ -33,5 +35,24 @@ module.exports = (function () {
         bot.say(to, "[IMDb] " + name + " " + year + " - " + rating + "/10");
       }
     });
+  };
+
+  function searchIMDb(search, bot, to) {
+    var request = require('request');
+    var cheerio = require('cheerio');
+    var url = "http://www.imdb.com/find?q=" + encodeURIComponent(search) + "&s=tt";
+
+    request(url, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+          var $ = cheerio.load(body);
+          var searchResult = /tt(\d{7})/.exec($('td.result_text a').attr('href'));
+          if (searchResult && searchResult[1]) {
+            getIMDb(searchResult[1], bot, to);
+          } else {
+            bot.say(to, "No search results for '" + search + "'.")
+          }
+      }
+    });
+
   };
 })();
