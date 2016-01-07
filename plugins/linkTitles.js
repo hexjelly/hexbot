11fe64a7848e5530bc5944d9ceb5ea7d4f31e687
Roger Andersen <hexjelly@hexjelly.com>
2016-01-07 12:37:10 +0100

    Remove consecutive spaces from titles

--- plugins/linkTitles.js
+++ plugins/linkTitles.js
@@ -13,13 +13,15 @@ module.exports = {
       var callback = params.callback;
       var url = (result[1] != 'www.' ? result[1] + result[2] : 'http://' + result[2]);
       var ignoreRegex = /^(?:.+\.)?([^\/\?]+\.[^\/\?]+)(?:\/|\?)?.*?$/i;
-      var ignore = ['youtube.com','youtu.be','wikipedia.org','imdb.com']; // because i'm too dumb to figure out some non-manual way to do this ._.
+      // because i'm too dumb to figure out some non-manual way to do this ._. ignore sites either handled by other plugins, or has poor titles
+      var ignore = ['youtube.com', 'youtu.be', 'wikipedia.org', 'imdb.com', 'facebook.com'];
       var host = ignoreRegex.exec(result[2]);
 
-      // var maxSize = 10485760; might be wise to limit a request attempt's size even if checking for content-type
       if (ignore.indexOf(host[1].toLowerCase()) === -1) {
         request({ url: url, method: "HEAD" }, function (err, headRes) {
-          /* var size = headRes.headers['content-length'];
+
+          /* var maxSize = 10485760; might be wise to limit a request attempt's size even if checking for content-type
+          var size = headRes.headers['content-length'];
           if (size > maxSize) {
               // too big
           } else {
@@ -33,12 +35,13 @@ module.exports = {
                 }
             }).pipe(file);
           } */
+
+          // only check for title of html files; indexOf because some sites send additional information in content-type header
           if (headRes && headRes.headers['content-type'].indexOf('text/html') > -1) {
             request(url, function (error, response, body) {
               if (!error && response.statusCode == 200) {
-                //console.log(response);
                 var $ = cheerio.load(body);
-                var title = $('title').text().replace(/(?:\r\n|\r|\n)/g, '');
+                var title = $('title').text().replace(/(?:\r\n|\r|\n|\s{2,})/g, ''); // remove newlines and multiple space characters
                 if (title) callback('[Title] ' + title);
               }
             });
