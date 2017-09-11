9e26b0ec9d28ce0b89348a65e3addd5ecb3c9e2a
Roger Andersen <hexjelly@hexjelly.com>
2017-09-11 13:50:00 +0200

    feat: change translate pluging to use google translate

--- plugins/translate.js
+++ plugins/translate.js
@@ -1,46 +1,23 @@
-/* jshint node: true */
-/* Translation using Yandex API */
-
 'use strict';
 
-var nconf = require('nconf');
-var request = require('request');
+const translate = require('google-translate-api');
 
 module.exports = {
   "message": {
     "regex": /^!tr\s+(?:\:([a-z]{2})\s*)?(?:\:([a-z]{2})\s*)?(.+)$/i,
     "handler": function (params) {
-      var APIKey = nconf.get('plugins').translate.APIKey;
-      var result = params.result;
-      var from = params.from;
-      var to = params.to;
-      var callback = params.callback;
-      // yandex API error codes
-      var codes = {
-        "200": "Operation completed successfully",
-        "401": "Invalid API key",
-        "402": "Blocked API key",
-        "403": "Exceeded the daily limit on the number of requests",
-        "404": "Exceeded the daily limit on the amount of translated text",
-        "413": "Exceeded the maximum text size",
-        "422": "The text cannot be translated",
-        "501": "The specified translation direction is not supported"
-      };
-      var language = (result[1] && !result[2] ? result[1] : (result[2] ? result[1] + '-' + result[2] : 'en'));
-      var translateText = result[3];
-      var url = "https://translate.yandex.net/api/v1.5/tr.json/translate?key=" + APIKey + "&text=" + encodeURIComponent(translateText) + "&lang=" + language + "&options=1";
+      const result = params.result;
+      const from = params.from;
+      const to = params.to;
+      const fromLang = !result[2] ? null : result[1];
+      const toLang = result[2] ? result[2] : (result[1] ? result[1] : 'en');
+      const callback = params.callback;
+      const translateText = result[3];
 
-      request(url, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-          body = JSON.parse(body);
-          if (body.code == 200) {
-            callback.say(to, from + ', ' + body.text);
-          } else {
-            callback.say(to, from + ', ' + body.code + ': ' + codes[body.code]);
-          }
-        } else {
-          callback.say(to, from + ", couldn't connect to translation API. Server responded with code " + response.statusCode);
-        }
+      translate(translateText, {from: fromLang, to: toLang}).then(res => {
+          callback.say(to, `${from}, [${fromLang ? fromLang : res.from.language.iso} => ${toLang}] ${res.text}`);
+      }).catch(err => {
+          callback.say(to, `${from}, ${err.message}`);
       });
     }
   }
