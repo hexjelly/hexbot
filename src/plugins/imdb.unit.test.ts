c4ee23ed5d07149958ed6452be88b8e63a3e199f
Roger Andersen <hexjelly@hexjelly.com>
2018-09-26 23:59:26 +0200

    test: add simple test for getIMDb function

--- src/plugins/imdb.unit.test.ts
+++ src/plugins/imdb.unit.test.ts
@@ -13,7 +13,7 @@ describe("IMDb plugin", () => {
 		expect(formatResponse(casablanca)).toBe("[IMDb] Casablanca (1942) 8.5/10");
 	});
 
-	test("gives correct title info", async () => {
+	test("gives correct title info when searching", async () => {
 		// intercept API call and reply with expected JSON
 		Nock("http://www.omdbapi.com")
 			.get("/")
@@ -27,5 +27,21 @@ describe("IMDb plugin", () => {
 			});
 		const res = await searchIMDb("Fake movie");
 		expect(res).toBe("[IMDb] Fake movie (2018) 10/10 https://www.imdb.com/title/tt9600051/");
+	});
+
+	test("gives correct title info when retrieving", async () => {
+		// intercept API call and reply with expected JSON
+		Nock("http://www.omdbapi.com")
+			.get("/")
+			.query(true)
+			.reply(200, {
+				"Title": "Fake movie",
+				"Year": "2018",
+				"imdbRating": "10",
+				"imdbID": "tt9600051",
+				"Response": "True"
+			});
+		const res = await getIMDb("Fake movie");
+		expect(res).toBe("[IMDb] Fake movie (2018) 10/10");
 	});
 });
