fb9148559f8c5fc0223df4eefe16d2fefde8f660
Roger Andersen <hexjelly@hexjelly.com>
2018-09-29 10:42:52 +0200

    feat: add currency plugin

--- src/bot.ts
+++ src/bot.ts
@@ -1,12 +1,13 @@
 import 'log-timestamp'; // adds datetimestamp to any console.* calls
 import Client from './client';
 
-import { IMDb, Logging, LinkTitle } from './plugins';
+import { IMDb, Logging, LinkTitle, Currency } from './plugins';
 
 const bot: any = new Client(); // no typings for irc-framework class :(
 
 bot.use(Logging());
 bot.use(IMDb());
+bot.use(Currency());
 bot.use(LinkTitle());
 
 export default bot;
--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,3 +1,4 @@
 export { Logging } from './logging/Logging';
 export { IMDb } from './imdb/IMDb';
 export { LinkTitle } from './linktitle/LinkTitle';
+export { Currency } from './currency/Currency';
