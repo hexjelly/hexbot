d0a05d8dcec7fc07646345bb038d3215b7e6a0af
Roger Andersen <hexjelly@hexjelly.com>
2018-09-29 11:58:40 +0200

    fix: add a more lenient max msg length

--- src/bot.ts
+++ src/bot.ts
@@ -5,6 +5,25 @@ import { IMDb, Logging, LinkTitle, Currency } from './plugins';
 
 const bot: any = new Client(); // no typings for irc-framework class :(
 
+// workaround for irc-framework, where not being able to connect
+// the first attempt when running it will make the process quit
+bot.connection.connected = true;
+bot.connection.registered = true;
+
+// irc-framework's default max msg length is extremely conservative
+// let's up it to 510 minus a VERY generous command + channel/target length
+bot.options.message_max_length = 510 - 35;
+
+bot.on('registered', () => {
+	const channels = process.env.CHANNELS ? process.env.CHANNELS.split(',') : [];
+	channels.forEach(channel => {
+		bot.join('#' + channel);
+	});
+});
+
+// bot.on('debug', event =>	console.log(event));
+// bot.on('raw', event => console.log(event));
+
 bot.use(Logging());
 bot.use(IMDb());
 bot.use(Currency());
