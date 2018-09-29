7e38d4656877dc0d99c4eafcfe5a893c5da266b3
Roger Andersen <hexjelly@hexjelly.com>
2018-09-29 05:35:59 +0200

    add: link title plugin

--- src/bot.ts
+++ src/bot.ts
@@ -1,11 +1,12 @@
 import 'log-timestamp'; // adds datetimestamp to any console.* calls
 import Client from './client';
 
-import { IMDb, Logging } from './plugins';
+import { IMDb, Logging, LinkTitle } from './plugins';
 
-const bot: any = new Client(); // no typings for irc-framework class
+const bot: any = new Client(); // no typings for irc-framework class :(
 
 bot.use(Logging());
 bot.use(IMDb());
+bot.use(LinkTitle());
 
 export default bot;
